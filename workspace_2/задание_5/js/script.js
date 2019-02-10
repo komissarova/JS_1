f2ddfa83a8fe830b01a9365910ae484a9ac39abf
komissarova <elya_komissarova@mail.ru>
2019-02-10 23:11:28 +0300

    Добавила файл js пятого задания

---
+++ workspace_2/задание_5/js/script.js
@@ -0,0 +1,41 @@
+﻿let arg1 = parseInt(prompt("Введите число: "));
+let arg2 = parseInt(prompt("Введите второе число: "));
+let operation = prompt("Укажите арифметическую операцию: ");
+
+function addNumbers() {
+  return arg1 + arg2;  
+}
+
+function subtractNumbers() {
+    return arg1 - arg2;    
+}
+
+function divideNumbers() {
+    return arg1 / arg2;    
+}
+
+function multiplyNumbers() {
+    return arg1 * arg2;   
+}
+
+function mathOperation() {
+  switch(operation){
+       case 'сложение':
+       case 'Сложение':
+         return addNumbers();
+         break;
+       case 'вычитание':
+       case 'Вычитание':
+         return subtractNumbers();
+         break;
+       case 'деление':
+       case 'Деление':
+         return divideNumbers();
+         break;
+       case 'умножение':
+       case 'Умножение':
+         return multiplyNumbers();
+         break;
+  }
+}
+alert(mathOperation(arg1, arg2, operation));
